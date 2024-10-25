796447981da78d96899d4c75ce460da54bff8d69
Jeremy Kang <jjk9914@ad.nyu.edu>
2024-10-25 17:19:49 -0400

    test000

--- src/project.v
+++ src/project.v
@@ -19,6 +19,7 @@ module tt_um_c13_array_mult (
   // All output pins must be assigned. If not used, assign to 0.
     wire [3:0] sum0, sum1, sum2, sum3, sum4, sum5, sum6;
     wire [3:0] c0, c1, c2, c3, c4, c5, c6;
+    wire [3:0] q, m;
     assign q = ui_in[3:0];
     assign m = ui_in[7:4];
     Node1 node0(c0[0], sum0[0], m[0], q[0], 1'b0, 1'b0);
@@ -46,10 +47,10 @@ module tt_um_c13_array_mult (
     assign uo_out[5] = sum5[3];
     assign uo_out[6] = sum6[3];
     assign uo_out[7] = c6[3];
-    assign uio_oe  = 0;
+    
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, uio_out, uio_in, 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_out, uio_in, uio_oe, 1'b0};
 
 endmodule
 
